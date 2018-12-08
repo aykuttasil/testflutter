4e525bd4c1e973233164de7b7519da32ef3fe67c
Aykut Asil <aykuttasil@gmail.com>
2018-12-08 17:10:36 +0300

    listview builder

---
+++ lib/widget/my-button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends RaisedButton {
+  @override
+  Widget build(BuildContext context) {
+    child:
+    Text('');
+    return super.build(context);
+  }
+}
