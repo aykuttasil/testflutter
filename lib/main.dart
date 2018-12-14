035cc5e680d27101dc379ee09aa5be7fb78a92cf
Aykut Asil <aykuttasil@gmail.com>
2018-12-15 02:18:25 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
@@ -12,10 +9,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Colors.blueAccent,
+        primaryColor: Colors.indigo,
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Test'),
+      home: MyHomePage(title: 'Flutter Http Test'),
     );
   }
 }
@@ -29,47 +26,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = TextStyle(fontSize: 18, color: Colors.black);
+  static String _url = "http://www.aykutasil.com";
+  String _sonuc;
 
-  Widget _buildSuggestionList() {
-    return ListView.builder(
-      padding: EdgeInsets.all(10),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      subtitle: Text('Aykut Asil'),
-      trailing: new Icon(
-        // Add the lines from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _sonuc = "test";
   }
 
   @override
@@ -77,53 +41,15 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )
-        ],
       ),
-      body: _buildSuggestionList(),
+      body: Text(_sonuc),
       floatingActionButton: FloatingActionButton(
-        onPressed: _loadData,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  _loadData() async {
-    String dataURL = "https://jsonplaceholder.typicode.com/posts";
-    http.Response response = await http.get(dataURL);
-    debugPrint("response");
-    /*
-    setState(() {
-      widgets = json.decode(response.body);
-    });
-    */
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> x = _saved.map((WordPair wordPair) {
-            return ListTile(
-              title: Text(wordPair.asPascalCase),
-            );
+        onPressed: () {
+          setState(() {
+            _sonuc = "";
           });
-
-          final Iterable<Widget> listTilesWidget = ListTile.divideTiles(
-            context: context,
-            tiles: x,
-          ).toList();
-
-          return Scaffold(
-              appBar: AppBar(
-                title: Text("Favori List"),
-              ),
-              body: ListView(children: listTilesWidget));
         },
+        child: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/myhttp.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyHttpWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
